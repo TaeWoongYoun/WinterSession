0b10f4e07fe29e4df1530a52b72bde1d4e43f476
TaeWoongYoun <24457545yong@gmail.com>
2025-12-23 12:59:10 +0900

    feat : Update project2 homework

--- C++/Project2/main.cpp
+++ C++/Project2/main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
 using namespace std;
 
+// Rectangle 생성자
+class Rectangle {
+public:
+	int width, height;
+	Rectangle();
+	Rectangle(int w, int h);
+	Rectangle(int length);
+	bool isSquare();
+};
+
+Rectangle::Rectangle() {
+	width = height = 1;
+}
+
+Rectangle::Rectangle(int w, int h) {
+	width = w;  height = h;
+}
+
+Rectangle::Rectangle(int length) {
+	width = height = length;
+}
+
+bool Rectangle::isSquare(){
+	return width == height;
+}
+
+int main() {
+	Rectangle rect1;
+	Rectangle rect2(3, 5);
+	Rectangle rect3(3);
+
+	if (rect1.isSquare()) cout << "rect1은 정사각형이다." << endl;
+	if (rect2.isSquare()) cout << "rect2은 정사각형이다." << endl;
+	if (rect3.isSquare()) cout << "rect3은 정사각형이다." << endl;
+}
+
 // 2개의 생성자를 가진 Circle 클래스
-class Circle {
+/*class Circle {
 public:
 	int radius;
 	Circle();
@@ -10,10 +46,7 @@ public:
 	double getArea();
 };
 
-Circle::Circle() {
-	radius = 1;
-	cout << "반지름 " << radius << " 원 생성" << endl;
-}
+Circle::Circle() : Circle(1) {}
 
 Circle::Circle(int r) {
 	radius = r;
@@ -32,7 +65,7 @@ int main() {
 	Circle pizza(30);
 	area = pizza.getArea();
 	cout << "pizza 면적은 " << area << endl;
-}
+}*/
 
 // Rectangle 클래스 만들기
 //class Rectangle {
