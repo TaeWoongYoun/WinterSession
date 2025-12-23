a853540b10cbac961145363da9e81b62fda76d6c
TaeWoongYoun <24457545yong@gmail.com>
2025-12-23 10:24:31 +0900

    feat : Create project 2 homework

--- C++/Project2/main.cpp
+++ C++/Project2/main.cpp
@@ -1,14 +1,55 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int nNum = 10;
+// 2개의 생성자를 가진 Circle 클래스
+class Circle {
+public:
+	int radius;
+	Circle();
+	Circle(int r);
+	double getArea();
+};
 
-	cout << "10진수 : " << nNum << endl;
-
-	cout << oct << "8진수 : " << nNum << endl;
-
-	cout << hex << "16진수 : " << nNum << endl;
-
-	return 0;
+Circle::Circle() {
+	radius = 1;
+	cout << "반지름 " << radius << " 원 생성" << endl;
 }
+
+Circle::Circle(int r) {
+	radius = r;
+	cout << "반지름 " << radius << " 원 생성" << endl;
+}
+
+double Circle::getArea() {
+	return 3.14 * radius * radius;
+}
+
+int main() {
+	Circle donut;
+	double area = donut.getArea();
+	cout << "donut 면적은 " << area << endl;
+
+	Circle pizza(30);
+	area = pizza.getArea();
+	cout << "pizza 면적은 " << area << endl;
+}
+
+// Rectangle 클래스 만들기
+//class Rectangle {
+//public:
+//	int width;
+//	int height;
+//	int getArea();
+//
+//};
+//
+//int Rectangle::getArea() {
+//	return width * height;
+//}
+//
+//int main() {
+//	Rectangle rect;
+//	rect.width = 3;
+//	rect.height = 5;
+//	cout << "사각형의 면적은 " << rect.getArea() << endl;
+//}
