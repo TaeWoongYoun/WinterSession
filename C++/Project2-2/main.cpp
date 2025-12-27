8df85014102e6338e8bf1a680f1db2828e971453
TaeWoongYoun <24457545yong@gmail.com>
2025-12-27 17:21:28 +0900

    feat : Study project 2-2

---
+++ C++/Project2-2/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+
+/*
+과제 1: Rectangle 클래스
+요구사항:
+
+멤버 변수: width, height (int 타입)
+멤버 함수:
+
+getArea() : 넓이 반환
+getPerimeter() : 둘레 반환
+
+
+선언부와 구현부를 분리하여 작성
+main에서 객체 생성 후 width=5, height=3 대입하고 결과 출력
+*/
+/*class Rectangle {
+public:
+    int width;
+    int height;
+    int getArea();
+    int getPerimeter();
+};
+
+int Rectangle::getArea() {
+    return width * height;
+}
+
+int Rectangle::getPerimeter() {
+    return 2 * (width + height);
+}
+
+int main() {
+    Rectangle test;
+    test.width = 5;
+    test.height = 3;
+    int result1 = test.getArea();
+    int result2 = test.getPerimeter();
+    cout << "면적은 : " << result1 << endl << "둘레는 : " << result2;
+    return 0;
+}*/
