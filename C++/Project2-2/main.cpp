b7bbe0cbd256640e448b5a0f4875f738890f9258
TaeWoongYoun <24457545yong@gmail.com>
2025-12-28 22:09:06 +0900

    test : Update file

--- C++/Project2-2/main.cpp
+++ C++/Project2-2/main.cpp
@@ -1,7 +1,81 @@
 #include <iostream>
 using namespace std;
 
-class Car {
+/*
+과제: Book 클래스
+요구사항:
+
+멤버 변수: title (string), author (string), price (int)
+생성자 3개 작성:
+
+기본 생성자: title="제목없음", author="저자미상", price=0
+title, author만 받는 생성자: price=0
+title, author, price 모두 받는 생성자
+
+
+멤버 함수: display() - 책 정보 출력
+*/
+
+class Book {
+public:
+    string title;
+    string author;
+    int price;
+    Book();
+    Book(string title, string author);
+    Book(string title, string author, int price);
+    void display();
+};
+
+Book::Book(){
+    title = "제목없음";
+    author = "저자미상";
+    price = 0;
+}
+
+Book::Book(string t, string a) {
+    title = t;
+    author = a;
+    price = 0;
+}
+
+Book::Book(string t, string a, int p) {
+    title = t;
+    author = a;
+    price = p;
+}
+
+void Book::display() {
+    cout << "제목 : " << title << ", " << "저자 : " << author << ", " << "가격 : " << price << endl;
+}
+
+int main() {
+    Book book1;
+    Book book2("C++ 프로그래밍", "윤태웅");
+    Book book3("자료구조", "홍길동", 25000);
+
+    book1.display();
+    book2.display();
+    book3.display();
+
+    return 0;
+}
+
+/*
+과제: Car 클래스
+요구사항:
+
+멤버 변수: brand (string), speed (int)
+생성자 3개 작성 (오버로딩):
+
+기본 생성자: brand="Unknown", speed=0 으로 초기화
+brand만 받는 생성자: speed=0 으로 초기화
+brand와 speed 모두 받는 생성자
+
+
+멤버 함수: printInfo() - 브랜드와 속도 출력
+*/
+/*class Car {
 public:
     string brand;
     int speed;
@@ -41,7 +115,9 @@ int main() {
     car3.printInfo();
 
     return 0;
-}
+}*/
+
+/*===================================================================================*/
 
 /*
 과제 1: Rectangle 클래스
