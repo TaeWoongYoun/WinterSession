06e41f56065d498424b15f652fef4032733969c0
TaeWoongYoun <24457545yong@gmail.com>
2025-12-29 10:15:27 +0900

    fix : Update

--- C++/Project4/main.cpp
+++ C++/Project4/main.cpp
@@ -5,7 +5,12 @@ int main() {
 	int cnt;
 	cout << "입력할 정수의 개수는? : ";
 	cin >> cnt;
+	if (cnt <= 0) return 0;
 	int* p = new int [cnt];
+	if (!p) {
+		cout << "메모리를 할당할 수 없습니다.";
+		return 0;
+	}
 
 	for (int i = 0; i < cnt; i++) {
 		cout << i + 1 << "번째 정수 : ";
