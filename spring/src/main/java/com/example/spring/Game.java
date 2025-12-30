fcb8f4156cc57c6ee86d3a21b5f0d71a983c8c6d
TaeWoongYoun <24457545yong@gmail.com>
2025-12-30 14:34:04 +0900

    feat : Test spring web app

--- spring/src/main/java/com/example/spring/Game.java
+++ spring/src/main/java/com/example/spring/Game.java
@@ -1,31 +1,43 @@
 package com.example.spring;
 
 public class Game {
-    //기능
-    /*
-    레벨업을 하면 속성을 3 올릴 수 있다.
-    죽으면 레벨이 1 내려가며 속성이 랜덤으로 3 깎인다
-    */
-/*========================================================*/
-    //속성
+
     private static final int MAX_LEVEL = 999;
     private String name; //이름
     private Role role; //직업
     private int power; //힘
     private int heart; //체력
-    private int speed; //이동속도
     private int defense; //방어력
-// /*========================================================*/
-    //동작
+    
+    // 생성자
+    public Game(String name, int heart, int power, int defense ,Role role) {
+        this.name = name;
+        this.heart = heart;
+        this.power = power;
+        this.defense = defense;
+        this.role = role;
+    }
+    // 공격을 한다
+    public void Attack(int power) {
+        int damage = power;
+    }
+
     // 공격을 당한다
-    // (어떠한 유저에게 데미지를 받아서 기존 체력에서 몇이 까인다.)
-    public void Attacked(int damage, String name, int heart) {
-        heart -= damage;
-        if (heart <= 0) {
-            System.out.println(name+"에게 사망함");
+    public String Attacked(String attackerName, int damage) {
+        this.heart -= damage - (this.defense / 10);
+        if (this.heart <= 0) {
+            return this.name + "이(가) " + attackerName + "에게 죽어버림";
         } else {
-            System.out.println(damage+"만큼의 공격을 받고"+heart+"의 체력이 남음");
+            return this.name + "이(가) " + damage + "만큼의 공격을 받고 " + this.heart + "의 체력이 남음";
         }
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public String getName() {
+        return name;
     }
 
 }
