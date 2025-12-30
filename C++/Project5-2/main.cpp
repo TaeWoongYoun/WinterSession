cdb126fe33002bce96fed5e532786f757e71bfa1
TaeWoongYoun <24457545yong@gmail.com>
2025-12-30 10:39:26 +0900

    feat : Update project 5-2 code

---
+++ C++/Project5-2/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle() { radius = 1; }
+	Circle(int radius) { this->radius = radius; }
+	void setRadius(int radius) { this->radius = radius; }
+	double getArea() { return 3.14 * radius * radius; }
+};
+
+void readRadius(Circle& d) {
+	int r;
+	cout << "정수 값으로 반지름을 입력하세요 >> ";
+	cin >> r;
+	d.setRadius(r);
+}
+
+int main() {
+	Circle donut;
+	readRadius(donut);
+	cout << "donut의 면적 = " << donut.getArea() << endl;
+	return 0;
+}
