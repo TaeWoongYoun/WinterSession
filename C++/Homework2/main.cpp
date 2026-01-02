78c69262f99ef540e581ed876848e4153bfb9775
TaeWoongYoun <24457545yong@gmail.com>
2026-01-02 09:51:36 +0900

    feat : Update homework2

---
+++ C++/Homework2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	char city[21];
+
+	cout << "이름을 입력하라 (20개 미만) ";
+	cin.getline(city, 21, '.');
+	cout << "이름은 " << city << endl;
+
+	return 0;
+}
