949742fa3ccc9b831468e9ddfd67dbece840d549
TaeWoongYoun <24457545yong@gmail.com>
2026-01-02 10:20:47 +0900

    feat : Update homework 4-1

---
+++ C++/Homework4-1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int Max(int* arr, int size) {
+    int max = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int* arr = new int[5];
+
+    cout << "5개 정수를 입력하세요 ";
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    int max = Max(arr, 5);
+    cout << "max = " << max << "입니다" << endl;
+
+    delete[] arr;
+    return 0;
+}
